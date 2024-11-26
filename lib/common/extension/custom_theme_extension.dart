1fb243b1f9d03150ca7e89dc66b51c7b1187e835
20200966 <20200966@miucateci.edu.do>
2024-11-26 03:16:06 -0400

    fix: extension

--- lib/common/extension/custom_theme_extension.dart
+++ lib/common/extension/custom_theme_extension.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_messenger/common/utils/coloors.dart';
+import 'package:whatsapp_clone_app/common/utils/coloors.dart';
 
 extension ExtendedTheme on BuildContext {
   CustomThemeExtension get theme {
