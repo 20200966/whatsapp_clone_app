2f7dc97923e343002824db99bca2956997daae8b
20200966 <20200966@miucateci.edu.do>
2024-11-26 10:17:12 -0400

    importing package

--- lib/feature/auth/widgets/custom_text_field.dart
+++ lib/feature/auth/widgets/custom_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone_app/common/extension/custom_theme_extension.dart';
 import 'package:whatsapp_clone_app/common/utils/coloors.dart';
 
 class CustomTextField extends StatelessWidget {
