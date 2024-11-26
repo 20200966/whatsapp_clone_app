b41c3b0d631f03c8f1fb6174fd2e8c919f62c391
20200966 <20200966@miucateci.edu.do>
2024-11-26 11:19:14 -0400

    importing packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone_app/feature/welcome/pages/welcome_page.dart';
 import 'package:whatsapp_clone_app/common/theme/dark_theme.dart';
 import 'package:whatsapp_clone_app/common/theme/light_theme.dart';
