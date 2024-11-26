310b365da2e9f590b041e2c19507ea098a819890
20200966 <20200966@miucateci.edu.do>
2024-11-26 11:34:25 -0400

    changes to main

--- lib/main.dart
+++ lib/main.dart
@@ -6,79 +6,47 @@ import 'package:whatsapp_clone_app/feature/welcome/pages/welcome_page.dart';
 import 'package:whatsapp_clone_app/common/theme/dark_theme.dart';
 import 'package:whatsapp_clone_app/common/theme/light_theme.dart';
 import 'package:whatsapp_clone_app/feature/auth/pages/login_page.dart';
+import 'package:whatsapp_clone_app/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
- 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp Me',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      home: ref.watch(userInfoAuthProvider).when(
+            data: (user) {
+              FlutterNativeSplash.remove();
+              if (user == null) return const WelcomePage();
+              return const HomePage();
+            },
+            error: (error, trace) {
+              return const Scaffold(
+                body: Center(
+                  child: Text('Something wrong happened'),
+                ),
+              );
+            },
+            loading: () => const SizedBox(),
+          ),
+      onGenerateRoute: Routes.onGenerateRoute,
     );
   }
 }
